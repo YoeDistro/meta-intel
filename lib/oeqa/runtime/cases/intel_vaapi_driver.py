1fe615d8faed1e9a31a51cee1e35b44fb3d74052
Naveen Saini <naveen.kumar.saini@intel.com>
2020-12-28 17:33:36 +0800

    intel_vaapi_driver.py: fix encoding pipeline
    Add missing h264parse in pipeline
    
    Error log:
    
    dd Pipeline is PREROLLING ...
    Got context from element 'vaapisink0': gst.vaapi.Display=context, gst.vaapi.Display=(GstVaapiDisplay)"\(GstVaapiDisplayDRM\)\ vaapidisplaydrm1";
    ERROR: from element /GstPipeline:pipeline0/GstQTDemux:qtdemux0: This file is corrupt and cannot be played.
    Additional debug info:
    ../gst-plugins-good-1.18.2/gst/isomp4/qtdemux.c(12726): qtdemux_parse_trak (): /GstPipeline:pipeline0/GstQTDemux:qtdemux0
    ERROR: pipeline doesn't want to preroll.
    ERROR: from element /GstPipeline:pipeline0/GstQTDemux:qtdemux0: This file contains no playable streams.
    
    No need to export i965 driver anymore.
    
    Signed-off-by: Naveen Saini <naveen.kumar.saini@intel.com>
    Signed-off-by: Anuj Mittal <anuj.mittal@intel.com>

--- lib/oeqa/runtime/cases/intel_vaapi_driver.py
+++ lib/oeqa/runtime/cases/intel_vaapi_driver.py
@@ -16,14 +16,12 @@ class VaapiDriverTest(OERuntimeTestCase):
         (status, output) = self.target.run('gst-inspect-1.0 vaapi')
         self.assertEqual(status, 0, msg='status and output: %s and %s' % (status, output))
 
-        (status, output) = self.target.run('export LIBVA_DRIVER_NAME=i965; '
-                                           'gst-launch-1.0 -ev videotestsrc num-buffers=60 ! '
-                                           'timeoverlay ! vaapih264enc ! mp4mux ! filesink location=/tmp/vtest_h264.mp4')
+        (status, output) = self.target.run('gst-launch-1.0 -ev videotestsrc num-buffers=60 ! '
+                                           'timeoverlay ! vaapih264enc ! h264parse ! mp4mux ! filesink location=/tmp/vtest_h264.mp4')
         self.assertEqual(status, 0, msg='status and output: %s and %s' % (status, output))
 
     @OETestDepends(['intel_vaapi_driver.VaapiDriverTest.test_gstreamer_can_encode_with_intel_vaapi_driver'])
     def test_gstreamer_can_decode_with_intel_vaapi_driver(self):
-        (status, output) = self.target.run('export LIBVA_DRIVER_NAME=i965; '
-                                           'gst-launch-1.0 filesrc location=/tmp/vtest_h264.mp4 ! '
+        (status, output) = self.target.run('gst-launch-1.0 filesrc location=/tmp/vtest_h264.mp4 ! '
                                            'qtdemux ! h264parse ! vaapih264dec ! vaapisink')
         self.assertEqual(status, 0, msg='status and output: %s and %s' % (status, output))
